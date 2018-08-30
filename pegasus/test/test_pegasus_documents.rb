08bc42af7742cba0792982a3ca2f243091e65bed
Brad Buchanan <brad@code.org>
2018-08-30 13:54:46 -0700

    Less parallel test_pegasus_documents.rb

--- pegasus/test/test_pegasus_documents.rb
+++ pegasus/test/test_pegasus_documents.rb
@@ -102,7 +102,7 @@ class PegasusTest < Minitest::Test
     DB.disconnect
     DASHBOARD_DB.disconnect
 
-    results = Parallel.map(all_documents) do |page|
+    results = Parallel.map(all_documents, in_processes: (Parallel.processor_count - 1)) do |page|
       site = page[:site]
       uri = page[:uri]
 
